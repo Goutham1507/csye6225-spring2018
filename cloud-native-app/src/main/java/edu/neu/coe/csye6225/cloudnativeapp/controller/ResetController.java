aed3e1dc33cd05fcfa767fea72514c4b22702c03
anubhav1011 <gupta.anu@husky.neu.edu>
2018-03-25 19:17:27 -0400

    Assignment 8: Adding Reset Service and sending message functionality

--- cloud-native-app/src/main/java/edu/neu/coe/csye6225/cloudnativeapp/controller/ResetController.java
+++ cloud-native-app/src/main/java/edu/neu/coe/csye6225/cloudnativeapp/controller/ResetController.java
@@ -1,14 +1,22 @@
 package edu.neu.coe.csye6225.cloudnativeapp.controller;
 
 import edu.neu.coe.csye6225.cloudnativeapp.User.ResetForm;
+import edu.neu.coe.csye6225.cloudnativeapp.service.ResetPasswordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.ExecutionException;
+
 @Controller
 public class ResetController {
+
+
+    @Autowired
+    ResetPasswordService resetPasswordService;
 
     @RequestMapping("/resetPassword")
     public String getResetPasswordPage(Model model) {
@@ -23,6 +31,13 @@ public class ResetController {
 
 
         System.out.println(resetForm.getEmailAddress());
+        try {
+            resetPasswordService.sendMessage(resetForm.getEmailAddress());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return  "resetPassword";
+        }
+
         return "resetPassword";
 
 
